5bfae089455a1c5bc8f88c0149d5d9ab51be9eee
Spencer Gray <spencer@mapletonhillmedia.com>
2016-07-07 14:31:53 -0600

    sortByProp

--- models/nutrition.js
+++ models/nutrition.js
@@ -347,18 +347,6 @@ var NutritionCollection = (function() {
             return 0;
         },
 
-        sortByName: function (a, b) {
-            if (a.name > b.name) {
-                return 1;
-            }
-
-            if (a.name < b.name) {
-                return -1;
-            }
-
-            return 0;
-        },
-
         recipeName: function (recipe) {
             var name = [];
 
